6e305a39ee330b01ae2bc2762ca4bcf50772d536
Leon Si <leonzalion@gmail.com>
2021-07-15 12:05:42 -0400

    added stuff

--- src/pages/PostQuestionPage.vue
+++ src/pages/PostQuestionPage.vue
@@ -1,16 +1,36 @@
 <template>
-  <q-btn label="Post Question" />
-  <q-editor v-model="editor"></q-editor>
+  <div class="q-ma-md column">
+    <div class="text-h3 text-center text-bold q-mb-sm">Post a Question</div>
+    <q-input
+      v-model="question"
+      class="q-mb-sm"
+      label="Question"
+      outlined
+      dense
+    />
+    <q-editor v-model="editor"></q-editor>
+    <q-btn label="Post" class="q-mt-md self-center" @click="postQuestion" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
+import { api } from 'src/utils';
 
 export default defineComponent({
   setup() {
-    const editor = ref();
+    const editor = ref('');
+    const question = ref('');
 
-    return { editor };
+    async function postQuestion() {
+      console.log(editor.value);
+      await api.post('question', {
+        question: question.value,
+        content: editor.value,
+      });
+    }
+
+    return { editor, question, postQuestion };
   },
 });
 </script>
